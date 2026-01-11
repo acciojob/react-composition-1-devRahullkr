196f953ec7779c6346d89a7330d71d13dcc5a57d
Rahulllkr <rahulkumar8604745@gmail.com>
2026-01-11 22:49:25 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [tab,setTab] = useState("")
+  const [tab,setTab] = useState(1)
   return (
     <div>
         {/* Do not remove the main div */}
