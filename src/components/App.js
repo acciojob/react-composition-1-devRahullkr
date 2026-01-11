7ee70ccfb5b0e0d537d712c242febe52955da353
Rahulllkr <rahulkumar8604745@gmail.com>
2026-01-11 23:09:34 +0530

    check again

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-        <h1>Hello world</h1>
+        <h1>Hello React Page</h1>
         <ul>
           <li onClick={(e) => setTab(1)}>Tab 1</li>
           <li onClick={(e) => setTab(2)}>Tab 2</li>
